5cc6eec3d060a07fd5bc7e71a2b8a4e91df8d2c7
Collin Kier <collinkier@live.com>
2020-01-12 14:03:51 -0800

    Got the HTML and CSS all squared away for the most part. Still need javascript

---
+++ appearance.js
@@ -0,0 +1,31 @@
+// create variables
+
+var randomPassword = document.querySelector("#password");
+var generatePassword = document.querySelector("#generate");
+var clipboardCopy = document.querySelector("#copy");
+// var passwordRange = document.querySelector()
+
+// create all attributes that will go into the page
+
+// Make an array of all of the values
+
+var randomCharacters = ["a","b","c","d","e","f","g","h","i","j","k",
+"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+"!","@","#","$","%","^","&","*","(","+","A","B","C","D","E","F","G","H","I","J","K",
+"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2",
+"3","4","5","6","7","8","9"];
+
+document.getElementById("#generate").addEventListener("click", function(){
+    console.log(randomCharacters[4]);
+});
+
+
+
+//   function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+//   }
+
+
+
+
+
