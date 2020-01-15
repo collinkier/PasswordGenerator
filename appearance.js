43fc10330df2e2d1a1858013fe619da4af0c9d40
Collin Kier <collinkier@live.com>
2020-01-14 18:19:11 -0800

    Got the javascript to work... still needs a lot of work, but functional

--- appearance.js
+++ appearance.js
@@ -3,9 +3,10 @@
 var randomPassword = document.querySelector("#password");
 var generatePassword = document.querySelector("#generate");
 var clipboardCopy = document.querySelector("#copy");
-// var passwordRange = document.querySelector()
+var output = document.querySelectorAll("placeholder");
+var password = "";
+var number = 0;
 
-// create all attributes that will go into the page
 
 // Make an array of all of the values
 
@@ -15,19 +16,32 @@ var randomCharacters = ["a","b","c","d","e","f","g","h","i","j","k",
 "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2",
 "3","4","5","6","7","8","9"];
 
-document.getElementById("#generate").addEventListener("click", function(){
-    console.log(randomCharacters[4]);
+var digits = prompt("pick a range between 8 and 128");
+
+generatePassword.addEventListener("click", function(){
+    // pick 4 letters
+    // loop through array
+    for( var i = 0; i < digits; i++){
+        number = getRandomInt(digits);
+        password += randomCharacters[number];
+         
+    }
+    // join letters
+    // print letters to console log
+    // console.log(password);
+    randomPassword.textContent = password;
 });
 
 
 
-//   function getRandomInt(max) {
-//     return Math.floor(Math.random() * Math.floor(max));
-//   }
 
 
+  function getRandomInt(max) {
+      console.log(digits);
+    return Math.floor(Math.random() * Math.floor(max));
+    
+  }
 
-YOOOOOOOOOOOOOOOO
 
 
 
