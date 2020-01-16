d2c65f344158dd03a69644fbb9a29d023550cd00
Collin Kier <collinkier@live.com>
2020-01-15 19:51:44 -0800

    Got the range slider to return a value... still cannot figure out how to use that value as the password length

--- appearance.js
+++ appearance.js
@@ -4,25 +4,47 @@ var randomPassword = document.querySelector("#password");
 var generatePassword = document.querySelector("#generate");
 var clipboardCopy = document.querySelector("#copy");
 var output = document.querySelectorAll("placeholder");
+var range = document.querySelector("#customRange1");
+var rangeLabel = document.querySelector("#textInput");
 var password = "";
 var number = 0;
 
 
 // Make an array of all of the values
 
-var randomCharacters = ["a","b","c","d","e","f","g","h","i","j","k",
-"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-"!","@","#","$","%","^","&","*","(","+","A","B","C","D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2",
-"3","4","5","6","7","8","9"];
+var randomCharacters = ["A","a","!","B","b","@","C","c","$","D","d","%",
+"E","e","^","F","f","&","G","g","(","+","H","h","#","I","i","!",
+"J","j","@","K","k","$","L","l","%","M","m","^","N","n","(","O","o","+",
+"p","p","#","Q","q","!","R","r","@","S","s","$","T","t","%","U","u",
+"^","V","v","&","W","w","*","X","x","(","Y","y","+","Z","z","#"];
 
-var digits = prompt("pick a range between 8 and 128");
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k",
+"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K",
+"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var speacialCase = ["!","@","$","%","^","&","(","+","#"];
+
+
+
+// document.createElement("li");
+// li.innerHTML = range.textContent;
+// document.rangeLabel.appendChild(li.textContent);
+
+function updateTextInput(val) {
+  document.getElementById('textInput').value=val; 
+}
+
+var digits = rangeLabel;
+
 
 generatePassword.addEventListener("click", function(){
     // pick 4 letters
     // loop through array
     for( var i = 0; i < digits; i++){
         number = getRandomInt(digits);
+        
         password += randomCharacters[number];
          
     }
@@ -31,6 +53,8 @@ generatePassword.addEventListener("click", function(){
     // console.log(password);
     randomPassword.textContent = password;
 });
+
+console.log();
 
 
 
