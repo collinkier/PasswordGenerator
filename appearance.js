82d67a41114c84cccbe92e64ea72b8fb167d7490
Collin Kier <collinkier@live.com>
2020-01-18 16:39:44 -0800

    Final Product

--- appearance.js
+++ appearance.js
@@ -1,70 +1,120 @@
-// create variables
+
 
 var randomPassword = document.querySelector("#password");
 var generatePassword = document.querySelector("#generate");
 var clipboardCopy = document.querySelector("#copy");
 var output = document.querySelectorAll("placeholder");
-var range = document.querySelector("#customRange1");
+var range = document.querySelector("#customRange3");
 var rangeLabel = document.querySelector("#textInput");
 var password = "";
+var pwdLessLastChar="";
 var number = 0;
+var digits = 15;
+var digitsStr = "15";
+var upperCaseCount=0;
+var lowerCaseCount=0;
+var specialCharCount=0;
 
 
 // Make an array of all of the values
 
-var randomCharacters = ["A","a","!","B","b","@","C","c","$","D","d","%",
-"E","e","^","F","f","&","G","g","(","+","H","h","#","I","i","!",
-"J","j","@","K","k","$","L","l","%","M","m","^","N","n","(","O","o","+",
-"p","p","#","Q","q","!","R","r","@","S","s","$","T","t","%","U","u",
-"^","V","v","&","W","w","*","X","x","(","Y","y","+","Z","z","#"];
+var allCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","@","$","%","&","#"];
 
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k",
-"l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var speacialCase = ["!","@","$","%","^","&","(","+","#"];
+var specialCase = ["!","@","$","%","&","#"];
 
+var count = 0;
 
-
-// document.createElement("li");
-// li.innerHTML = range.textContent;
-// document.rangeLabel.appendChild(li.textContent);
 
 function updateTextInput(val) {
-  document.getElementById('textInput').value=val; 
-}
-
-var digits = rangeLabel;
-
+   digitsStr = val;
+   document.getElementById('textInput').value=val;
+  }  //terminates updateTextInput function
 
 generatePassword.addEventListener("click", function(){
+   //alert(specialCase.length);
+   password="";
+   //isFetchCharUpper=0;
+   upperCaseCount=0;
+   lowerCaseCount=0;
+   specialCharCount=0;
+   digits = digitsStr;
     // pick 4 letters
     // loop through array
     for( var i = 0; i < digits; i++){
-        number = getRandomInt(digits);
+        listlength=allCharacters.length;
+        number = getRandomInt(listlength);
+        fetchCharacter = allCharacters[number];
+        password=password+fetchCharacter;
+        upper_case(fetchCharacter);
+        lower_case(fetchCharacter);
         
-        password += randomCharacters[number];
+       }  //terminates for loop
+
+       specialCharCount=digits-upperCaseCount-lowerCaseCount;
+
+       if (specialCharCount == 0) {
          
-    }
-    // join letters
-    // print letters to console log
-    // console.log(password);
+         pwdLessLastChar = password.substring(0,digits-1);
+         listlength=specialCase.length;
+         number = getRandomInt(listlength);
+         fetchCharacter = specialCase[number];
+         password=pwdLessLastChar+fetchCharacter;
+       }
+       if (lowerCaseCount == 0) {
+         
+        pwdLessLastChar = password.substring(0,digits-1);
+        listlength=lowerCase.length;
+        number = getRandomInt(listlength);
+        fetchCharacter = lowerCase[number];
+        password=pwdLessLastChar+fetchCharacter;
+       
+     }
+
+     if (upperCaseCount == 0) {
+        pwdLessLastChar = password.substring(0,digits-1);
+        listlength=upperCase.length;
+        number = getRandomInt(listlength);
+        fetchCharacter = upperCase[number];
+        password=pwdLessLastChar+fetchCharacter;
+      
+     
+   }
+     
     randomPassword.textContent = password;
-});
 
-console.log();
-
-
-
+  
+});     /////closes parenthesis and terminates function for generatePassword()
 
 
   function getRandomInt(max) {
-      console.log(digits);
     return Math.floor(Math.random() * Math.floor(max));
-    
+   }
+
+
+
+
+  function upper_case(str)
+  {
+     regexp = /^[A-Z]/;
+       if (regexp.test(str))
+      {
+        upperCaseCount=upperCaseCount+1;
+      } 
+   }
+  
+  function lower_case(str)
+  {
+     regexp = /^[a-z]/;
+       if (regexp.test(str))
+      {
+        lowerCaseCount=lowerCaseCount+1;
+      } 
   }
+
 
 
 
